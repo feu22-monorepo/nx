a877d00cf65849433807d48f65faab6262a8034e
Alexander MP <alex.martos@hotmail.se>
2024-02-27 15:19:41 +0100

    Add label to component lib

---
+++ packages/ui/components/Label/Label.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Label } from "./index";
+
+const meta: Meta<typeof Label> = {
+  component: Label,
+};
+
+export default meta;
+type Story = StoryObj<typeof Label>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => (
+    <div>
+      <Label htmlFor="test">Test</Label>
+      <input id="test" />,
+    </div>
+  ),
+};
---
+++ packages/ui/components/Label/index.tsx
@@ -0,0 +1,15 @@
+import { label } from "./Label.module.css";
+
+export function Label({
+  children,
+  htmlFor,
+}: {
+  children: React.ReactNode;
+  htmlFor: string;
+}) {
+  return (
+    <label htmlFor={htmlFor} className={label}>
+      {children}
+    </label>
+  );
+}
